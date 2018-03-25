deb74c91e62c752da3b7dc39de2b732a4dd2c663
Andrew Phung <andrewphung@wireless-10-147-33-10.public.utexas.edu>
2018-03-24 21:25:03 -0500

    split package into variables

--- Python Script/serial_test.py
+++ Python Script/serial_test.py
@@ -1,18 +1,34 @@
 import serial
-import MySQLdb
+# import MySQLdb
 
 ser = serial.Serial('/dev/tty.usbmodemL2000IS1', 9600)
 ser.readline()
-db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                     user="root",         # your username
-                     passwd="password",  # your password
-                     db="SeniorDesign")        # name of the data base
-cur = db.cursor()
+# db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+#                      user="root",         # your username
+#                      passwd="password",  # your password
+#                      db="SeniorDesign")        # name of the data base
+# cur = db.cursor()
 while True:
     packet = ser.readline()
-    try:
-        with db.cursor() as cur:
-            cur.execute("SELECT * FROM YOUR_TABLE_NAME")
-    finally:
-        db.close()
-    print packet
+    (rawTemp, 
+    rawObjTemp, 
+    m, 
+    e, 
+    g_u32ActualPress, 
+    g_s32ActualTemp, 
+    g_u32ActualHumity, 
+    s_accelXYZ_x, 
+    s_accelXYZ_y, 
+    s_accelXYZ_z, 
+    s_gyroXYZ_x, 
+    s_gyroXYZ_y, 
+    s_gyroXYZ_z, 
+    s_magcompXYZ_x, 
+    s_magcompXYZ_y, 
+    s_magcompXYZ_z) = packet[1:][:70].split(",")
+    # try:
+    #     with db.cursor() as cur:
+    #         cur.execute("SELECT * FROM YOUR_TABLE_NAME")
+    # finally:
+    #     db.close()
+    print packet[1:][:70].split(",")
